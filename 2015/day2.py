4d8c5ee08d1e92cc9e2a47c2d360c55fb9952eaa
Skygal44 <maxencebodo@hotmail.fr>
2024-11-07 23:24:22 +0100

    Day2

--- 2015/day2.py
+++ 2015/day2.py
@@ -1,4 +1,27 @@
-with open('./2015/input2.txt') as file : 
-    text = file.read()
+with open('./input/input2.txt') as file : 
+    text = file.readlines()
 
 #Partie 1
+paper = 0
+for line in text : 
+    l,w,h = line.split('x')
+    l = int(l)
+    w = int(w)
+    h = int(h)
+    paper = paper + 2*l*w+2*w*h+2*h*l+min(l*w,w*h,h*l)
+    
+
+print(paper)
+
+#Partie 2
+ribbon = 0
+for line in text : 
+    l,w,h = line.split('x')
+    l = int(l)
+    w = int(w)
+    h = int(h)
+    smallest = sorted([l,w,h])[:2]
+
+    ribbon = ribbon + 2*smallest[0]+2*smallest[1] + l*w*h
+
+print(ribbon)
